e87df391c24928c39864c861adc96179a7c63a46
mdhack0316 <39210514+mdhack0316@users.noreply.github.com>
2024-08-20 18:21:50 +0530

    Update master.sh

--- master.sh
+++ master.sh
@@ -2,19 +2,13 @@
 
 swapoff -a
 
-
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
 
-
-
-
 modprobe overlay
 modprobe br_netfilter
-
-
 
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
@@ -25,44 +19,16 @@ EOF
 
 sysctl --system
 
-OS=xUbuntu_20.04
-
-VERSION=1.23
-
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key add -
-
-echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
-
-
 curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | sudo apt-key add -
 
-
-apt update
-
-
-apt install cri-o cri-o-runc cri-tools -y  && systemctl enable --now crio 
-
-systemctl start crio.service
-
-
-apt-get update && apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-
-
-
-
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-
-
-
-
-
+apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
-
 apt-get install -y kubelet kubeadm kubectl
+apt install cri-o cri-o-runc cri-tools -y  && systemctl enable --now crio 
+systemctl start crio.service
+apt-get update && apt-get install -y apt-transport-https
+apt-get update
 
 kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=0.0.0.0  --apiserver-cert-extra-sans=publicip  --control-plane-endpoint=publicip
