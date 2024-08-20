75ca7ad4122923d0b4cf950f6096e6fe6373b3e8
mdhack0316 <39210514+mdhack0316@users.noreply.github.com>
2024-08-20 18:30:20 +0530

    Update master.sh

--- master.sh
+++ master.sh
@@ -19,8 +19,6 @@ EOF
 
 sysctl --system
 
-curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | sudo apt-key add -
-
 apt-get install -y apt-transport-https ca-certificates curl gpg
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
